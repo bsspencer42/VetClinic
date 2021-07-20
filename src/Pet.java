f665a7277f383d0274bb302fdbebf7fd6b8458a9
bsspencer42 <bsspencer42@gmail.com>
2021-07-20 12:51:42 -0400

    Complete pet method

---
+++ src/Pet.java
@@ -0,0 +1,59 @@
+public abstract class Pet {
+    // Instance variables
+    private String name;
+    private double health;
+    private int painLevel;
+
+    // Constructor
+    public Pet(String name, double health, int painLevel){
+        if (health > 1.0){
+            health = 1.0;
+        }
+        else if (health < 0){
+            health = 0;
+        }
+        if (painLevel > 10){
+            painLevel = 10;
+        }
+        else if (painLevel < 1){
+            painLevel = 1;
+        }
+        this.name = name;
+        this.health = health;
+        this.painLevel = painLevel;
+    }
+
+    // Getters
+    public String getName(){
+        return this.name;
+    }
+    public double health(){
+        return this.health;
+    }
+    public int painLevel(){
+        return this.painLevel;
+    }
+
+    // Abstract Methods
+    abstract int treat();
+
+    // Methods
+    public void speak(){
+        System.out.println("Hello! My name is " + this.name);
+    }
+
+    public void heal(){
+        this.health = 1.0;
+        this.painLevel = 1;
+    }
+
+    // Overrides
+    @Override
+    public boolean equals(Object o){
+        if (o instanceof Pet){
+            Pet otherPet = (Pet) o;
+            return this.name == otherPet.getName();
+        }
+        return false;
+    }
+}
